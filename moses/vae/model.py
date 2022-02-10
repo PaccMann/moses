d7109d32588b7053511b3e16e6ba3632613bc312
Ashish Dave <Ashish.Dave1@ibm.com>
2022-02-10 11:43:35 +0000

    fix: removed .cpu() methods for lengths variable

--- moses/vae/model.py
+++ moses/vae/model.py
@@ -127,7 +127,7 @@ class VAE(nn.Module):
 
         z_0 = z.unsqueeze(1).repeat(1, x_emb.size(1), 1)
         x_input = torch.cat([x_emb, z_0], dim=-1)
-        x_input = nn.utils.rnn.pack_padded_sequence(x_input, lengths.cpu(),
+        x_input = nn.utils.rnn.pack_padded_sequence(x_input, lengths,
                                                     batch_first=True)
 
         h_0 = self.decoder_lat(z)
